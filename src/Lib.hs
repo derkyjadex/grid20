35a526ab29626c1748e9f9763d60e5987b91afb7
James Deery <james.deery@jadex.org>
2015-07-24 19:54:43 +0100

    Whitespace and formatting changes

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,39 +12,34 @@ data Direction
   = Horizontal
   | Vertical
   | Diagonal
-  deriving (Eq,Ord,Show)
+  deriving (Eq, Ord, Show)
 
 type Grid = Array (Int, Int) Integer
-type Signpost = (Int,Int,Direction)
+type Signpost = (Int, Int, Direction)
 
 makeGrid :: StdGen -> Grid
 makeGrid stdGen =
-  array ((0,0),(19,19))
+  array ((0, 0), (19, 19))
         (zip positions values)
-  where positions =
-          [(x,y) | x <- [0 .. 19]
-                 , y <- [0 .. 19]]
-        values = randomRs (0,100) stdGen
+  where positions = [(x, y) | x <- [0..19], y <- [0..19]]
+        values = randomRs (0, 100) stdGen
 
-prettyRow :: Grid -> Set (Int,Int) -> Int -> String
+prettyRow :: Grid -> Set (Int, Int) -> Int -> String
 prettyRow grid highlighted row =
-  concat [printf (format col) $
-          grid !
-          (col,row) | col <- [0 .. 19]]
+  concat [printf (format col) $ grid ! (col, row) | col <- [0..19]]
   where format col =
-          if (Set.member (row,col) highlighted)
-             then "\ESC[41m%4d\ESC[40m"
+          if Set.member (row, col) highlighted
+             then " \ESC[41m%3d\ESC[40m"
              else "%4d"
 
-prettyGrid :: Grid -> Set (Int,Int) -> String
+prettyGrid :: Grid -> Set (Int, Int) -> String
 prettyGrid grid highlighted =
-  unlines [prettyRow grid highlighted row | row <-
-                                             [0 .. 19]]
+  unlines [prettyRow grid highlighted row | row <- [0..19]]
 
 nextPosition :: Signpost -> Signpost
-nextPosition (x,y,d@Horizontal) = (x + 1,y,d)
-nextPosition (x,y,d@Vertical) = (x,y + 1,d)
-nextPosition (x,y,d@Diagonal) = (x + 1,y + 1,d)
+nextPosition (x, y, d@Horizontal) = (x + 1, y, d)
+nextPosition (x, y, d@Vertical) = (x, y + 1, d)
+nextPosition (x, y, d@Diagonal) = (x + 1, y + 1, d)
 
 slice :: Grid -> Int -> Signpost -> [Integer]
 slice _ 0 _ = []
@@ -56,12 +51,12 @@ slice grid count signpost@(x,y,_) =
 
 possibleValues :: [Signpost]
 possibleValues =
-  [(x,y,Horizontal) | x <- [0 .. 16]
-                    , y <- [0 .. 19]] ++
-  [(x,y,Vertical) | x <- [0 .. 19]
-                  , y <- [0 .. 16]] ++
-  [(x,y,Diagonal) | x <- [0 .. 16]
-                  , y <- [0 .. 16]]
+  [(x, y, Horizontal) | x <- [0..16]
+                    , y <- [0..19]] ++
+  [(x, y, Vertical) | x <- [0..19]
+                  , y <- [0..16]] ++
+  [(x, y, Diagonal) | x <- [0..16]
+                  , y <- [0..16]]
 
 findSolution :: Grid -> (Signpost, Integer)
 findSolution grid =
